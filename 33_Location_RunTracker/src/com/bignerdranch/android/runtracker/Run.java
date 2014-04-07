bf76131a1bd861e8ec23511d299b9fa246bebe30
ajkwak <ajkwak@users.noreply.github.com>
2014-04-07 09:45:02 -0700

    Add javadoc to Run class

--- 33_Location_RunTracker/src/com/bignerdranch/android/runtracker/Run.java
+++ 33_Location_RunTracker/src/com/bignerdranch/android/runtracker/Run.java
@@ -2,35 +2,81 @@ package com.bignerdranch.android.runtracker;
 
 import java.util.Date;
 
+/**
+ * A class representing a single run being tracked by this application.
+ *
+ * @author Bill Phillips
+ * @author Brian Hardy
+ * @author ajkwak@users.noreply.github.com (AJ Parmidge)
+ */
 public class Run {
     private long mId;
     private Date mStartDate;
 
+    /**
+     * Creates a new {@code Run} with an invalid ID. By default, the start date of the newly created
+     * run is set to the time at which this constructor is called.
+     */
     public Run() {
         mId = -1;
         mStartDate = new Date();
     }
 
+    /**
+     * Gets the ID of this run.
+     *
+     * @return the ID of this run
+     */
     public long getId() {
         return mId;
     }
 
+    /**
+     * Sets the unique ID of this run to the given value.
+     *
+     * @param id the ID to set
+     */
     public void setId(long id) {
         mId = id;
     }
 
+    /**
+     * Gets the start date of this run.
+     *
+     * @return the start date of this run
+     */
     public Date getStartDate() {
         return mStartDate;
     }
 
+    /**
+     * Sets the start date of this run to the given value.
+     *
+     * @param startDate the start date to set
+     */
     public void setStartDate(Date startDate) {
         mStartDate = startDate;
     }
 
+    /**
+     * Gets the duration of this run, in milliseconds, with the given run end time.
+     *
+     * @param endMillis the end time of the run (in millisecods)
+     * @return the duration of the run (in milliseconds)
+     */
     public int getDurationSeconds(long endMillis) {
         return (int)((endMillis - mStartDate.getTime()) / 1000);
     }
 
+    /**
+     * Formats the given run duration as an easily human-readable string with the format
+     * {@code HH:MM:SS} (where {@code HH} is the number of hours in the run, {@code MM} is the
+     * number of minutes in the run, and {@code SS} is the number of seconds in the run).
+     *
+     * @param durationSeconds the duration of the run, in seconds
+     * @return the string representation of the run duration, formatted as
+     *         [hours]:[minutes]:[seconds]
+     */
     public static String formatDuration(int durationSeconds) {
         int seconds = durationSeconds % 60;
         int minutes = ((durationSeconds - seconds) / 60) % 60;
