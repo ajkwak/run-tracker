3644997d3f789388a2187c629ccb328301cef2c6
ajkwak <ajkwak@users.noreply.github.com>
2014-04-09 15:47:42 -0700

    Add package-info

---
+++ 33_Location_RunTracker/src/com/bignerdranch/android/runtracker/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains classes used for tracking runs a person takes with the GPS of their Android device.
+ *
+ * @author ajkwak@users.noreply.github.com (AJ Parmidge)
+ */
+package com.bignerdranch.android.runtracker;
